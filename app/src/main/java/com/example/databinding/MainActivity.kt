900caee31a216394f492336ae8373f8a12f22dc8
Md shah samir <shahsamir97@users.noreply.github.com>
2020-05-02 12:38:53 +0600

    Initial commit

---
+++ app/src/main/java/com/example/databinding/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.databinding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.databinding.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+    private var videoGame :VideoGame = VideoGame("GTA V","100")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.videoGame = videoGame
+    }
+}
